529d65a400950e3f585e0403ee1a5ab11a26d64e
Daniel Stockton <91233809+Ghost99er@users.noreply.github.com>
2024-04-26 11:12:51 -0600

    first commit

---
+++ Week 1/hello world.py
@@ -0,0 +1,35 @@
+#Overview
+
+#Project Title: Hello World (Python)
+
+#Project Description: A simple "Hello World" program in Python
+
+#Project Goals: Have program run and show Hello World
+
+#Instructions for Build and Use
+
+#Steps to build and/or run the software:
+
+#Ensure you have Python installed on your system
+#Copy the provided Python code into a file named hello_world.py
+#Open a terminal or command prompt.
+#Navigate to the directory where hello_world.py is saved.
+#Run the program by executing the command: python hello_world.py
+
+#Instructions for using the software:
+
+#After running the program, you will see "Hello, World!" printed to the console.
+#To recreate the development environment, you need the following software and/or libraries with the specified versions:
+#Python (version 3.x)
+
+# Useful Websites to Learn More
+
+#I found these websites useful in developing this software:
+
+#python.org - Official website for downloading Python
+
+#CODE
+
+# Print "Hello, World!" to the console
+print("Hello, World!")
+
