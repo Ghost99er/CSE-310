525da0d3433fe1eb823ae01c63f089f74cb3c001
Daniel Stockton <91233809+Ghost99er@users.noreply.github.com>
2024-05-06 17:15:12 -0600

    5.6.2024 update

---
+++ Java project/MovieRecommender.java
@@ -0,0 +1,235 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class MovieRecommender {
+
+    static class Movie {
+        String title;
+        String genre;
+        int rating;
+
+        public Movie(String title, String genre, int rating) {
+            this.title = title;
+            this.genre = genre;
+            this.rating = rating;
+        }
+    }
+
+    static class User {
+        String name;
+        String preferredGenre;
+        ArrayList<Movie> watchedHistory;
+
+        public User(String name, String preferredGenre) {
+            this.name = name;
+            this.preferredGenre = preferredGenre;
+            this.watchedHistory = new ArrayList<>();
+        }
+
+        public void watchMovie(Movie movie) {
+            watchedHistory.add(movie);
+        }
+
+        public void showWatchHistory() {
+            if (watchedHistory.isEmpty()) {
+                System.out.println("Your watch history is empty.");
+            } else {
+                System.out.println("Watch history for " + name + ":");
+                for (Movie movie : watchedHistory) {
+                    System.out.println("* " + movie.title + " (" + movie.genre + ")");
+                }
+            }
+        }
+    }
+
+    static Scanner scanner = new Scanner(System.in);
+    static HashMap<String, User> users = new HashMap<>();
+
+    static ArrayList<Movie> recommendMovies(ArrayList<Movie> movies, String genre) {
+        ArrayList<Movie> recommendations = new ArrayList<>();
+        for (Movie movie : movies) {
+            if (movie.genre.equals(genre)) {
+                recommendations.add(movie);
+            }
+        }
+        return recommendations;
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Movie> movies = createMovieList();
+        movies.add(new Movie("12 Angry Men", "Drama", 5));
+        movies.add(new Movie("2001: A Space Odyssey", "Science Fiction", 5));
+        movies.add(new Movie("A Beautiful Mind", "Biography", 4));
+        movies.add(new Movie("A Clockwork Orange", "Crime", 4));
+        movies.add(new Movie("A Few Good Men", "Drama", 4));
+        movies.add(new Movie("A Fistful of Dollars", "Western", 4));
+        movies.add(new Movie("A Hard Day's Night", "Comedy", 4));
+        movies.add(new Movie("A Nightmare on Elm Street", "Horror", 4));
+        movies.add(new Movie("A Streetcar Named Desire", "Drama", 5));
+        movies.add(new Movie("Alien", "Science Fiction", 5));
+        movies.add(new Movie("American History X", "Drama", 5));
+        movies.add(new Movie("Apocalypse Now", "War", 5));
+        movies.add(new Movie("Avatar", "Science Fiction", 4));
+        movies.add(new Movie("Back to the Future", "Science Fiction", 5));
+        movies.add(new Movie("Blade Runner", "Science Fiction", 5));
+        movies.add(new Movie("Braveheart", "History", 5));
+        movies.add(new Movie("Casablanca", "Romance", 5));
+        movies.add(new Movie("Catch Me If You Can", "Biography", 4));
+        movies.add(new Movie("Chinatown", "Mystery", 5));
+        movies.add(new Movie("City of God", "Crime", 5));
+        movies.add(new Movie("Die Hard", "Action", 5));
+        movies.add(new Movie("Django Unchained", "Western", 5));
+        movies.add(new Movie("Doctor Zhivago", "Romance", 4));
+        movies.add(new Movie("Donnie Darko", "Drama", 4));
+        movies.add(new Movie("Dr. Strangelove", "Comedy", 5));
+        movies.add(new Movie("E.T. the Extra-Terrestrial", "Science Fiction", 5));
+        movies.add(new Movie("Fargo", "Crime", 5));
+        movies.add(new Movie("Forrest Gump", "Drama", 4));
+        movies.add(new Movie("Full Metal Jacket", "War", 5));
+        movies.add(new Movie("Gone with the Wind", "Romance", 5));
+        movies.add(new Movie("Goodfellas", "Crime", 4));
+        movies.add(new Movie("Gravity", "Science Fiction", 4));
+        movies.add(new Movie("Heat", "Crime", 4));
+        movies.add(new Movie("Inception", "Science Fiction", 4));
+        movies.add(new Movie("Indiana Jones and the Last Crusade", "Action", 5));
+        movies.add(new Movie("Inglourious Basterds", "War", 5));
+        movies.add(new Movie("Interstellar", "Science Fiction", 5));
+        movies.add(new Movie("It's a Wonderful Life", "Drama", 5));
+        movies.add(new Movie("Jaws", "Thriller", 5));
+        movies.add(new Movie("Jurassic Park", "Science Fiction", 4));
+        movies.add(new Movie("Kill Bill: Vol. 1", "Action", 4));
+        movies.add(new Movie("La La Land", "Musical", 4));
+        movies.add(new Movie("Lawrence of Arabia", "History", 5));
+        movies.add(new Movie("Life is Beautiful", "Comedy", 5));
+        movies.add(new Movie("Mad Max: Fury Road", "Action", 5));
+        movies.add(new Movie("Memento", "Mystery", 5));
+        movies.add(new Movie("No Country for Old Men", "Crime", 5));
+        movies.add(new Movie("Once Upon a Time in the West", "Western", 5));
+        movies.add(new Movie("One Flew Over the Cuckoo's Nest", "Drama", 5));
+        movies.add(new Movie("Pan's Labyrinth", "Fantasy", 5));
+        movies.add(new Movie("Parasite", "Thriller", 5));
+        movies.add(new Movie("Pulp Fiction", "Crime", 4));
+        movies.add(new Movie("Raiders of the Lost Ark", "Action", 5));
+        movies.add(new Movie("Requiem for a Dream", "Drama", 5));
+        movies.add(new Movie("Rocky", "Drama", 4));
+        movies.add(new Movie("Saving Private Ryan", "War", 5));
+        movies.add(new Movie("Schindler's List", "History", 5));
+        movies.add(new Movie("Seven Samurai", "Adventure", 5));
+        movies.add(new Movie("Shawshank Redemption", "Drama", 5));
+        movies.add(new Movie("Singin' in the Rain", "Musical", 5));
+        movies.add(new Movie("Snatch", "Crime", 4));
+        movies.add(new Movie("Some Like It Hot", "Comedy", 5));
+        movies.add(new Movie("Star Wars: Episode IV - A New Hope", "Science Fiction", 5));
+        movies.add(new Movie("Terminator 2: Judgment Day", "Science Fiction", 5));
+        movies.add(new Movie("The Dark Knight", "Action", 5));
+        movies.add(new Movie("The Exorcist", "Horror", 5));
+        movies.add(new Movie("The Godfather", "Crime", 5));
+        
+
+        
+        // Loop to remove duplicates
+  for (int i = movies.size() - 1; i >= 0; i--) {
+    Movie movie = movies.get(i);
+    boolean foundDuplicate = false;
+    for (int j = 0; j < i; j++) {
+      if (movies.get(j).equals(movie)) {
+        foundDuplicate = true;
+        break;
+      }
+    }
+    if (foundDuplicate) {
+      movies.remove(i);
+    }
+  }
+
+
+        while (true) {
+            System.out.println("Are you a new user or a returning user? (new/return)");
+            String userType = scanner.nextLine();
+
+            if (userType.equalsIgnoreCase("new")) {
+                System.out.println("Enter your name:");
+                String userName = scanner.nextLine();
+                System.out.println("What genre do you prefer (Crime, Drama, Action, Fantasy, Science Fiction, Thriller, History, Animation, Biography, Mystery, Romance, War, Western, Comedy)?");
+                String preferredGenre = scanner.nextLine();
+
+                User newUser = new User(userName, preferredGenre);
+                users.put(userName, newUser);
+
+                recommendAndWatchMovies(movies, newUser);
+
+            } else if (userType.equalsIgnoreCase("return")) {
+                System.out.println("Select your name:");
+                for (String userName : users.keySet()) {
+                    System.out.println("* " + userName);
+                }
+                String userName = scanner.nextLine();
+                if (users.containsKey(userName)) {
+                    User returningUser = users.get(userName);
+                    recommendAndWatchMovies(movies, returningUser);
+                } else {
+                    System.out.println("User not found. Please try again.");
+                }
+            } else {
+                System.out.println("Invalid input. Please enter 'new' or 'return'.");
+            }
+
+            System.out.println("Do you want to go back to menu? (yes/no)");
+            String continueChoice = scanner.nextLine();
+            if (!continueChoice.equalsIgnoreCase("yes")) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    static ArrayList<Movie> createMovieList() {
+        ArrayList<Movie> movies = new ArrayList<>();
+        // Add movies here
+        return movies;
+    }
+
+    static void recommendAndWatchMovies(ArrayList<Movie> movies, User user) {
+        while (true) {
+            ArrayList<Movie> recommendedMovies = recommendMovies(movies, user.preferredGenre);
+            if (recommendedMovies.isEmpty()) {
+                System.out.println("No movies found in your preferred genre.");
+            } else {
+                System.out.println("Here are some recommendations for you, " + user.name + ":");
+                for (Movie movie : recommendedMovies) {
+                    System.out.println("* " + movie.title + " (" + movie.genre + ")");
+                }
+                System.out.println("Did you watch any movie from the recommendation? (yes/no)");
+                String watchedChoice = scanner.nextLine();
+                if (watchedChoice.equalsIgnoreCase("yes")) {
+                    System.out.println("Enter the title of the movie you watched:");
+                    String watchedMovieTitle = scanner.nextLine();
+                    for (Movie movie : recommendedMovies) {
+                        if (movie.title.equalsIgnoreCase(watchedMovieTitle)) {
+                            user.watchMovie(movie);
+                            System.out.println("Added " + watchedMovieTitle + " to your watched history.");
+                            break;
+                        }
+                    }
+                }
+            }
+    
+            System.out.println("Do you want to see your watch history? (yes/no)");
+            String watchHistoryChoice = scanner.nextLine();
+            if (watchHistoryChoice.equalsIgnoreCase("yes")) {
+                user.showWatchHistory();
+            }
+    
+            System.out.println("Do you want new recommendations? (yes/no)");
+            String newRecommendationChoice = scanner.nextLine();
+            if (!newRecommendationChoice.equalsIgnoreCase("yes")) {
+                break;
+            }
+            
+            System.out.println("What genre do you prefer (Crime, Drama, Action, Fantasy, Science Fiction, Thriller, History, Animation, Biography, Mystery, Romance, War, Western, Comedy)?");
+            user.preferredGenre = scanner.nextLine();
+        }
+    }
+}    
---
+++ Java project/read me.md
@@ -0,0 +1,28 @@
+# Movie Recommender System
+
+This is a Java program that recommends movies to users based on their preferred genre. Users can watch recommended movies and view their watch history.
+
+## Implementation Details
+
+### Variables
+- The program uses variables to store user information such as name and preferred genre, as well as movie information such as title, genre, and rating.
+
+### Expressions
+- Expressions are used throughout the program for various purposes, such as checking conditions and generating recommendations.
+
+### Conditionals
+- Conditionals are used to handle different scenarios, such as determining whether a user is new or returning, and whether to continue recommending movies.
+
+### Loops
+- Loops are used for iterating over movie lists, checking for duplicates, and providing a continuous user interface loop.
+
+### Functions
+- Functions are used to encapsulate behavior such as watching movies, showing watch history, and recommending movies.
+
+### Classes
+- The program defines two classes: `Movie` and `User`, to represent movies and users, respectively.
+
+### Data Structure (Java Collection Framework)
+- The program utilizes ArrayList and HashMap from the Java Collection Framework to store movies and user information.
+
+
